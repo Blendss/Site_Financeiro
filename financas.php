21b052f04b29e24d7299a4b7656f83b524c00e07
zJoaoGabriel <gabriel_sasaki03@hotmail.com>
2023-12-02 01:53:55 -0300

    financas não achei o bug que o thomas falou
    botão de switch dark mode padronizado.
    gráfico agora usa a cor da tela branca mesmo no dark mode.
    removido botão "financas" do menu.
    código formatado depois de sangrar meus olhos tentando achar onde começava e terminava o switch.

--- financas.php
+++ financas.php
@@ -188,7 +188,7 @@ echo "<script>let JS_Array = ". $js_array . ";</script>";
 
 
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script type="text/javascript">
+<script type="text/javascript">
     google.charts.load("current", {packages:['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
@@ -211,7 +211,7 @@ echo "<script>let JS_Array = ". $js_array . ";</script>";
 
       var options = {
         title: "Divisão de gastos",
-         'backgroundColor': 'transparent',
+         'backgroundColor': '#f1f1f1',
         // width: 500,
         height: 400,
         bar: {groupWidth: "95%"},
@@ -243,84 +243,83 @@ echo "<script>let JS_Array = ". $js_array . ";</script>";
 						</label>
 					</div>
 				</li>
-				<li><div class="box"> <input style="margin: 0px 0px" class="cb-input" type="checkbox" id="switch"><label class="cb-label" for="switch"></label>
-				<div class="moon"><div class="gg-sun"></div></div> </div> </li>
                 <li><a>Conta</a></li>
                 <li><a>Bancos</a></li>
                 <li><a>Configuraçôes</a></li>
                 <li><a href="extrato.php">Extrato</a></li>
-                <li><a href="financas.php">Finanças</a></li>
                 <li><a href="Login.php">Sair</a></li>
             </ul>
         </nav>
     </div>
 <body style="margin: 0; overflow-x: hidden;">
 <script src="index.js"></script>
-		<ul>
-			<li><img src="img/profileplaceholder.png" id="upload-img" width="120" height="120" style="margin:20px 10px"></li>
-			<li><table style="margin: 40px 0px">
-				<tr><td><h2>Olá, <?php echo $nome; ?></h2></td></tr>
-				
-				<tr><td><h3>Você está no: </h3>  
-					
+	<ul>
+		<li><img src="img/profileplaceholder.png" id="upload-img" width="120" height="120" style="margin:20px 10px"></li>
+		
+		<li>
+			<table style="margin: 40px 0px">
+			<tr>
+				<td>
+					<h2>Olá, <?php echo $nome; ?></h2>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<h3>Você está no: </h3>  
+					<form  action="" method="POST">
+						<select onchange="this.form.submit()" name="procure" class="select-bancoss" aria-label="Default select example">
+							<?php 
+							$sql_code = "SELECT * FROM usuariobancos JOIN bancos ON usuariobancos.cod = bancos.cod WHERE id = $id ORDER BY usuariobancos.cod;";
+							$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);		
+							if(isset($_POST['flag'])){
+								echo "//////////////////////////////////////". $_POST['procure'];
+								$primeirobanco = $_POST['procure'];
+							}else{$primeirobanco = '9999';}
+							echo "oioioioioio". $primeirobanco;
+							while($dados = $sql_query->fetch_array()){
+								if($primeirobanco == '9999'){
+									echo "<option selected style='font-weight: bold;' value='".$dados['cod']."'>".$dados['banco']."</option>";
+								}elseif($primeirobanco == $dados['cod']){
+									echo "<option selected style='font-weight: bold;' value='".$dados['cod']."'>".$dados['banco']."</option>";
+								}else{
+									echo "<option style='font-weight: bold;' value='".$dados['cod']."'>".$dados['banco']."</option>";
+								}
+								if ($primeirobanco == '9999'){
+									$primeirobanco = $dados['cod'];
+								}	
+							}
+							$_SESSION['cod_banco'] = $primeirobanco;
+							?>
+						</select>
+						<input type="hidden" name="flag" value="1" />
+					</form>
+				</td>
+			</tr>
+			</table>
+		</li>
 
-				<form  action="" method="POST">
-				<select onchange="this.form.submit()" name="procure" class="select-bancoss" aria-label="Default select example">
-					<?php 
-				
-					$sql_code = "SELECT * FROM usuariobancos JOIN bancos ON usuariobancos.cod = bancos.cod WHERE id = $id ORDER BY usuariobancos.cod;";
-					$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);		
-					
-					if(isset($_POST['flag'])){
-						echo "//////////////////////////////////////". $_POST['procure'];
-
-						$primeirobanco = $_POST['procure'];
-					}else{$primeirobanco = '9999';}
-					
-					echo "oioioioioio". $primeirobanco;
-					
-					while($dados = $sql_query->fetch_array()){
-						if($primeirobanco == '9999'){
-							echo "<option selected style='font-weight: bold;' value='".$dados['cod']."'>".$dados['banco']."</option>";
-						}elseif($primeirobanco == $dados['cod']){
-							echo "<option selected style='font-weight: bold;' value='".$dados['cod']."'>".$dados['banco']."</option>";
-						}else{
-							echo "<option style='font-weight: bold;' value='".$dados['cod']."'>".$dados['banco']."</option>";
-						}
-							
-						
-						if ($primeirobanco == '9999'){
-							$primeirobanco = $dados['cod'];
-						}	
-						}
-						$_SESSION['cod_banco'] = $primeirobanco;
-					?>
-				</select>
-				<input type="hidden" name="flag" value="1" />
-				</form>
-
-</td></tr>
-			</table></li>
-			<li><img
-				style="margin:30px 300px"
-				src="img/Screenshot_1.png"
-				width="450"
-				height="100"
-				align="middle"
-			/></li>
-		</ul>
+		<li>
+			<img
+			style="margin:30px 300px"
+			src="img/Screenshot_1.png"
+			width="450"
+			height="100"
+			align="middle"
+			/>
+		</li>
+	</ul>
 		<!-- <div style="right: 10px" class="chartcs" id="chart_div"></div> -->
-        <div style="margin: 61px 0px">
-	<div class="backtittlee">
-	<h2 class="tab" style="color: #fafafa;">Visão Financeira</h2>
-	</div>
-	</div>
-    <br>
-	
+	<div style="margin: 61px 0px">
+		<div class="backtittlee">
+			<h2 class="tab" style="color: #fafafa;">Visão Financeira</h2>
+		</div>
+	</div><br>
+
 	<table style="text-align:center; padding-top: 0px; margin: -10px 0px" class="table-extrato" border = 0 CELLSPACING=0 CELLPADDING=5. >
 		<tr style="text-align:center;" style="border-bottom: 10px">
-		<th width="5200px"><label class="label-bancos"></label></th>
-		<th style="border-radius: 0px 30px 30px 0px;" width="0px"><label class="label-bancos"></label></th>
+			<th width="5200px"><label class="label-bancos"></label></th>
+			<th style="border-radius: 0px 30px 30px 0px;" width="0px"><label class="label-bancos"></label></th>
 		</tr>
 	</table>
 
@@ -335,5 +334,4 @@ echo "<script>let JS_Array = ". $js_array . ";</script>";
 			</select>
 		</form>
 	</div>
-	<div style="right: 10px" class="chartcs" id="chart_div"></div>
-	<div style="right: 10px" class="chartcs" id="chart_div2"></div>
+	<div style="right: 10px" class="" id="chart_div"></div>
